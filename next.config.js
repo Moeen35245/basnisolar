1169a6e3c59418823b020d176fb44972cdc8834d
Moeen35245 <70256504+Moeen35245@users.noreply.github.com>
2022-06-06 22:10:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
   env: {
     USER_NAME: "moeen8766",
     PASSWORD: "mohammed8766",
+    NEXTAUTH_SECRET="b6bd3584d629f50d3c217fc6b3ee674ab2f7b00de1f9898940ad9b7c27b4509d78cd22b5f96a54f2f03b1133ff0f5857f8bc1bd792780a8269dc31bdd7ca87c4"
   },
 };
